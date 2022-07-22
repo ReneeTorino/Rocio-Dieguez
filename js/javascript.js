1d11ae4c8090ed1507bfa79b4bcc49974db9ca2e
ReneeTorino <104397108+ReneeTorino@users.noreply.github.com>
2022-07-22 14:39:32 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,7 @@
 
 
 function autor() {
-  document.getElementById("rocio").innerHTML = "© 2022 Rocío Dieguez / todos los derechos reservados";
+  document.getElementById("rocio").innerHTML="© 2022 Rocío Dieguez / todos los derechos reservados";
 }
-function cambiarfondo() {
-  document.body.style.background = "none";
-  document.body.style.backgroundColor = "black";
-  document.getElementById("titulo").style.color = "white";
-document.getElementById("fala").style.color = "white";
-document.getElementById("hysto").style.color = "white";
-document.getElementById("ingredientes").style.color = "white";
-document.getElementById("preparacion").style.color = "white";
-document.getElementById("elementos").style.color = "white";
-document.getElementById("mas").style.color = "white";
-document.getElementById("inicio").style.color = "white";
-document.getElementById("contactos").style.color = "white";
-document.getElementById("musica").style.color = "white";
-}
-function cambiarfondo1() {
-  document.body.style.background = "none";
-  document.body.style.backgroundColor = "white";
-  document.getElementById("titulo").style.color = "black";
-document.getElementById("fala").style.color = "black";
-document.getElementById("hysto").style.color = "black";
-document.getElementById("ingredientes").style.color = "black";
-document.getElementById("preparacion").style.color = "black";
-document.getElementById("elementos").style.color = "black";
-document.getElementById("hacer").style.color = "black";
-document.getElementById("mas").style.color = "black";
-document.getElementById("inicio").style.color = "black";
-document.getElementById("contactos").style.color = "black";
-document.getElementById("musica").style.color = "black";
-}
+
+function cambiarfondo()
