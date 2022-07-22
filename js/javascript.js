e0d325b5d98dabe87cdc8b738fed0923649ce84d
ReneeTorino <104397108+ReneeTorino@users.noreply.github.com>
2022-07-22 15:23:02 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -4,4 +4,30 @@ function autor() {
   document.getElementById("rocio").innerHTML="© 2022 Rocío Dieguez / todos los derechos reservados";
 }
 
-function cambiarfondo()
+
+function nightmode(){
+  document.body.style.backgroundColor = "black";
+  document.getElementById("titulogrande").style.color = "white";
+  document.getElementById("titulof").style.color = "white";
+  document.getElementById("textohistoria").style.color = "white";
+  document.getElementById("ingre").style.color = "white";
+  document.getElementById("prep").style.color = "white";
+  document.getElementById("hacer").style.color = "white";
+  document.getElementById("lista").style.color = "white";
+
+
+
+}
+
+
+function daymode(){
+
+    document.body.style.backgroundColor = "#ebbe9b";
+    document.getElementById("titulogrande").style.color = "black";
+    document.getElementById("titulof").style.color = "black";
+      document.getElementById("textohistoria").style.color = "black";
+      document.getElementById("ingre").style.color = "black";
+    document.getElementById("prep").style.color = "black";
+    document.getElementById("hacer").style.color = "black";
+    document.getElementById("lista").style.color = "black";
+    }
